150920dfc6e2c5a35ad6b49d733a7fd1fcbc09b4
limFakson <fakeyejoshua2005@gmail.com>
2023-10-06 17:54:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import { Heading } from '@chakra-ui/react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello World!
+    </>
+  )
+}
+
+export default App
